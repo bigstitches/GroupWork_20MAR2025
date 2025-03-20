b5fc9b0442c62012d3f8dad008e0db02586142f0
eileen beck <eileen.e.beck@gmail.com>
2025-03-20 10:36:05 -0700

    progress

--- GroupWork_20MAR2025/Program.cs
+++ GroupWork_20MAR2025/Program.cs
@@ -4,13 +4,54 @@ using System.Collections.Generic;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, People!");
+LongestConsecutiveSequence.Main();
 
 
 // wow! "commit all and sync" and I saw Lepolean's on my Visual Studio app
 
 public class LongestConsecutiveSequence
 {
-    public int LongestConsecutive(int[] nums)
+    public static void Main()
+    {
+        int[] nums = { 100, 3, 2, 0, 55, 1, 22 };
+        Console.WriteLine(LongestConsecutive(nums));
+    }
+
+    public static int LongestConsecutiveWithMap(int[] nums)
+    {
+        if (nums.Length < 2) { return nums.Length; }
+
+        // unique elements, not stored in any order, but takes O(1) time to retrieve/add/remove/contains
+
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+        foreach (int n in nums)
+
+        {
+            dictionary.Add(n, 0); // skip duplicates
+        }
+
+        int maxConsecutive = 0;
+
+        foreach (KeyValuePair<int, int> kvp in dictionary)
+        {
+            int counter = 1;
+            int firstElement = kvp.Key;
+            while (dictionary.ContainsKey(firstElement + 1))
+            {
+                firstElement += 1;
+                counter += 1;
+            }
+
+            if (counter > maxConsecutive)
+            {
+                maxConsecutive = counter;
+            }
+        }
+
+        return maxConsecutive;
+    }
+    public static int LongestConsecutive(int[] nums)
     {
         if (nums.Length < 2) { return nums.Length; }
 
